7c1dcaa859d76a459df11b9f40ee1007e67c9b15
theringmaster <gzroms@hotmail.com>
2017-05-09 20:10:34 -0400

    Validus

--- validus.mk
+++ validus.mk
@@ -2,8 +2,9 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common TESLA stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+# Inherit some common Validus stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
@@ -11,7 +12,7 @@ $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-gzr
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := tesla_shamu
+PRODUCT_NAME := validus_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 
@@ -19,5 +20,6 @@ TARGET_VENDOR := moto
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
+    DEVICE_MAINTAINERS="John Brewer(TheRingMaster)" \
     BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F26Y/3821691:user/release-keys \
     PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F26Y 3821691 release-keys"
