9a0697e5e97897a64c703c7f487f097a51876e19
Pranav Vashi <neobuddy89@gmail.com>
2017-07-03 21:12:31 +0530

    shamu: Enable HW acceleration
    Change-Id: I16f9c60d39ed93639950700dbd4d88112e8645c4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -214,6 +214,11 @@ PRODUCT_PACKAGES += \
     libxml2
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.egl.hw=1 \
+    debug.sf.hw=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    persist.hwc.enable_vds=1 \
+    sdm.perf_hint_window=50 \
     ro.opengles.version=196610
 
 PRODUCT_PROPERTY_OVERRIDES += \
