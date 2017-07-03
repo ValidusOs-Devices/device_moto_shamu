acdd486186d2ce258de148ff44432c9f3a7d2c44
Pranav Vashi <neobuddy89@gmail.com>
2017-07-03 21:14:36 +0530

    shamu: Enable full-fledged offload support
    * Now that kernel supports it!
    
    Change-Id: I075f4ddb22dfab07274824e25ac766e94c5eb66a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -451,7 +451,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicecomm=false \
     persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=false
+    persist.audio.fluence.speaker=false \
+    audio.offload.buffer.size.kb=32 \
+    audio.offload.gapless.enabled=true \
+    audio.offload.video=true \
+    audio.offload.pcm.16bit.enable=true \
+    audio.offload.pcm.24bit.enable=true
 
 # OEM Unlock reporting
 ADDITIONAL_DEFAULT_PROPERTIES += \
