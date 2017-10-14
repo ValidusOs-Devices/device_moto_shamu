66a8b13b594157ea00e6244e8ff32b995f2e1ff5
camcory <camcory1@gmail.com>
2017-10-14 09:10:47 -0400

    Build dumpstate HAL
    Change-Id: I9b2ddf932270adbccfb30cdb65abd628f4a2b23d

--- device.mk
+++ device.mk
@@ -109,6 +109,10 @@ PRODUCT_PACKAGES += \
     libbt-vendor \
     android.hardware.bluetooth@1.0-impl
 
+# Dumpstate HAL
+PRODUCT_PACKAGES += \
+    android.hardware.dumpstate@1.0-service.shamu
+
 # For SPN display
 PRODUCT_COPY_FILES += \
     device/moto/shamu/spn-conf.xml:system/etc/spn-conf.xml
